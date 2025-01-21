7f4f43e42cd6937dc6395da6a21a33dd5a01daa1
Lucas Gruner <lucas.gruner@gmail.com>
2025-01-21 22:17:48 +0100

    basic script for reading a YAML data and rendering HTML, with partials

---
+++ resume.rb
@@ -0,0 +1,21 @@
+require 'yaml'
+require 'erb'
+require 'webrick'
+
+# render method similar to Rails for partials
+def render(partial, locals = {})
+  template = File.read("views/_#{partial}.html.erb")
+  ERB.new(template).result_with_hash(locals)
+end
+
+# Read templates and data
+layout = File.read("views/layout.html.erb")
+view = File.read("views/view.html.erb")
+data = YAML.load_file('data.yml')
+
+# Hydrate the ERB template and insert the view in the layout
+rendered_view = ERB.new(view).result_with_hash(data)
+result = ERB.new(layout).result_with_hash(view: rendered_view)
+
+# Save result to a file
+File.write("public/index.html", result)
