9178506598e7c884f8b9bf1feccffcf04574022d
Lucas Gruner <lucas.gruner@gmail.com>
2025-02-28 19:36:31 +0100

    Add Grover PDF generation, refactor helper methods, reorganise

---
+++ lib/helpers.rb
@@ -0,0 +1,12 @@
+# Render a partial (usage similar to Rails)
+def render(partial, locals = {})
+template = File.read("views/_#{partial}.html.erb")
+ERB.new(template).result_with_hash(locals)
+end
+
+# Render an SVG file
+def svg_tag(file, options = {})
+  svg_content = File.read("assets/icons/#{file}.svg")
+  attributes = options.map { |key, value| %(#{key}="#{value}") }.join(" ")
+  svg_content.sub!("<svg", "<svg #{attributes}")
+end
---
+++ script.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+require 'erb'
+require 'grover'
+
+require_relative 'lib/helpers'
+
+# Parse data and read templates
+data = YAML.load_file('data.yml')
+layout = File.read("views/layout.html.erb")
+style = File.read("assets/style.css")
+view = File.read("views/view.html.erb")
+
+# Hydrate the ERB template and insert the view in the layout
+rendered_view = ERB.new(view).result_with_hash(data)
+result_html = ERB.new(layout).result_with_hash(view: rendered_view, style: style)
+
+# Save resulting HTML to a file
+File.write("public/index.html", result_html)
+puts "Generated public/index.html"
+
+# Use Grover to print a PDF
+result_pdf = Grover.new(result_html, format: 'A4').to_pdf
+File.write("public/resume.pdf", result_pdf)
+puts "Generated public/index.pdf"
